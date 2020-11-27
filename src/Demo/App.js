7353babc06371c9e47435fbd44478067621c362c
Good luck <2924456233@qq.com>
2020-11-27 21:14:11 +0800

    增加 udeOTP Demo

---
+++ src/Demo/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import OTP from "./OTP"
+
+function App() {
+    return (
+        <div className="App">
+            <OTP/>
+        </div>
+    );
+}
+
+export default App;
